3949272d982f15c78f2bea7d28b1edd2d7914166
Forrest Erickson <forresterickson@comcast.net>
2021-03-05 17:06:33 -0500

    Report program version. Add function to read all ADC into row of array. Print out array in setup.

--- AverageADCsRead.ino
+++ AverageADCsRead.ino
@@ -11,11 +11,17 @@
 */
 
 const String PROGRAMNAME = "AverageADCsRead";
+const String VERSION = "0.0.1";
 
 //For ADC indexing
-int muxNumber[] = {A0, A1, A2, A3, A4, A5};
-int MAXADC = sizeof(muxNumber) / sizeof(muxNumber[0]);
-const long SAMPLE_PERIOD = 300; // in mSec.
+const int MUX_NUMBER[] = {A0, A1, A2, A3, A4, A5};              //The Arduino UNO ADC inputs.
+const int MAXADC = sizeof(MUX_NUMBER) / sizeof(MUX_NUMBER[0]);  //The maximum mux number for ADC input.
+const long SAMPLE_PERIOD = 300;                                 // in mSec.
+const int AVG_NUMBER = 4;                                       //Number of samples to average
+
+//Array of measurements. Columbs are ADC measurements A0, A1...
+//rows are aged measurements
+int measurementsADC[AVG_NUMBER][MAXADC];
 
 
 //For Wink LED.
@@ -29,25 +35,57 @@ long nextLEDchange = 100; //time in ms.
 //For serial setup
 const long BAUDRATE = 115200;
 
+//Function to read all ADC into a row.
+void readADC(int rowIndex){ 
+    for (int adcIndex =0; adcIndex < MAXADC; adcIndex++) { 
+      measurementsADC[rowIndex][adcIndex] = analogRead(MUX_NUMBER[adcIndex]); //Get analog
+    }//adcIndex 
+  return;
+}//readADC
+ 
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(ON_BOARD_LED, OUTPUT);
   digitalWrite(ON_BOARD_LED, HIGH);
   Serial.begin(BAUDRATE);
   delay(1000);                  //Arbitrary wate.
+
+//Fill the array with successive measurements fast as we can.
+  for (int avgNumIndex = 0; avgNumIndex < AVG_NUMBER; avgNumIndex++) {
+    readADC(avgNumIndex);
+  }//avgNumIndex
+
   Serial.print("Begin: ");
-  Serial.println(PROGRAMNAME);
-  Serial.print("muxNumber: ");
+  Serial.print(PROGRAMNAME);
+  Serial.print(", Version: ");
+  Serial.println(VERSION);
+  
+  Serial.print("MUX_NUMBER: ");
   Serial.println(MAXADC);
   Serial.println("TIME, A0,A1,A2,A3,A4,A5 ");
+  
+//Print the array
+  for (int avgNumIndex = 0; avgNumIndex < AVG_NUMBER; avgNumIndex++) {
+    Serial.print(avgNumIndex); Serial.print(": ");
+    for (int adcIndex =0; adcIndex < MAXADC; adcIndex++) { 
+      Serial.print(measurementsADC[avgNumIndex][adcIndex]); 
+      Serial.print(", "); 
+    }//adcIndex 
+    Serial.println();
+  }//avgNumIndex
+    
 }//end setup
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Get Analog inputs
   Serial.print(millis());
   for (int ii = 0; ii < MAXADC ; ii++) {
     Serial.print(", ");
-    Serial.print(analogRead(muxNumber[ii]));
+    Serial.print(analogRead(MUX_NUMBER[ii]));
   }
   Serial.println();
   delay(SAMPLE_PERIOD);
